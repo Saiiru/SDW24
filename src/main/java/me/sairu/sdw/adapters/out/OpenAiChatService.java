d75942efd62cbffabd907bf72671a61de860a871
[SAIRU] <[sairufsa@gmail.com]>
2024-06-11 16:44:02 -0300

    crashing

--- src/main/java/me/sairu/sdw/adapters/out/OpenAiChatService.java
+++ src/main/java/me/sairu/sdw/adapters/out/OpenAiChatService.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@ConditionalOnProperty(name = "generative-ai.provider", havingValue = "OPENAI", matchIfMissing = true)
-@FeignClient(name = "openAiApi", url = "${openai.base-url}", configuration = OpenAiChatService.Config.class)
+@ConditionalOnProperty(name = "generative-ai.provider", havingValue = "OPENAI")
+@FeignClient(name = "openai", url = "${openai.base-url}", configuration = OpenAiChatService.class)
 public interface OpenAiChatService extends GenerativeAiService {
 
     @PostMapping("/v1/chat/completions")
